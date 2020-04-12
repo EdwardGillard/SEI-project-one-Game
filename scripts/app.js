69e1f7284d756a4fdd8aad8e4d9d5f41bd860322
Edward Gillard <edwardgillard@edwards-mbp.mynet>
2020-04-12 14:33:38 +0100

    added record

--- scripts/app.js
+++ scripts/app.js
@@ -7,52 +7,112 @@ function init() {
   const squares = []
 
   //GridInfo
-  const width = 20
-  const numberOfCells = width * width
+  const width = 10
+  const numberOfSquares = width * width
 
   let drakePosition = 0
+  let movePlease
+  let recordPosition = 0
+  let scoreCount = 0
+  let beganGame = false
+  let recordCount = 0
 
 
   //* functions
 
-  function createTheBoard() {
-    for (let i = 0; i < numberOfCells; i++) {
+  function createTheBoard(startPosition) {
+    for (let i = 0; i < numberOfSquares; i++) {
       const square = document.createElement('div')
       gameBoard.appendChild(square)
       square.textContent = i
       squares.push(square)
     }
-    squares[drakePosition].classList.add('drake')
+    squares[startPosition].classList.add('drake')
   }
 
+  function runTheTrap(event) {
+    if (event.keyCode === 32) {
+      if (beganGame) return
+      console.log('game began')
+      beganGame = true
+      recordPosition = Math.floor(Math.random() * numberOfSquares)
+      squares[recordPosition].classList.add('record')
+    }
+  }
+
+
+
   function handleMovements(event) {
-    squares[drakePosition].classList.remove('drake')
     const x = drakePosition % width
     const y = Math.floor(drakePosition / width)
     switch (event.keyCode) {
       case 39:
-        if (x < width - 1) drakePosition++
+        if (x < width - 1) {
+          clearInterval(movePlease)
+          movePlease = setInterval(() => {
+            squares[drakePosition].classList.remove('drake')
+            drakePosition++
+            squares[drakePosition].classList.add('drake')
+            recordsCaught(drakePosition)
+          }, 500)
+        }
         break
       case 37:
-        if (x > 0) drakePosition--
+        if (x > 0) {
+          clearInterval(movePlease)
+          movePlease = setInterval(() => {
+            squares[drakePosition].classList.remove('drake')
+            drakePosition--
+            squares[drakePosition].classList.add('drake')
+            recordsCaught(drakePosition)
+          }, 500)
+        }
         break
       case 38:
-        if (y > 0) drakePosition -= width
+        if (y > 0) {
+          clearInterval(movePlease)
+          movePlease = setInterval(() => {
+            squares[drakePosition].classList.remove('drake')
+            drakePosition -= width
+            squares[drakePosition].classList.add('drake')
+            recordsCaught(drakePosition)
+          }, 500)
+        }
         break
       case 40:
-        if (y < width - 1) drakePosition += width
+        if (y < width - 1) {
+          clearInterval(movePlease)
+          movePlease = setInterval(() => {
+            squares[drakePosition].classList.remove('drake')
+            drakePosition += width
+            squares[drakePosition].classList.add('drake')
+            recordsCaught(drakePosition)
+          }, 500)
+        }
         break
       default:
-        console.log('invalid keys')
+        // console.log('invalid keys')
+        // } console.log(drakePosition)
+        squares[drakePosition].classList.add('drake')
     }
-    squares[drakePosition].classList.add('drake')
   }
 
+  function recordsCaught(drakePosition) {
+    if (squares[drakePosition].classList.contains('record')) {
+      squares[recordPosition].classList.remove('record')
+      recordCount++
+      scoreCount += 10
+      recordPosition = Math.floor(Math.random() * numberOfSquares)
+      squares[recordPosition].classList.add('record')
+      console.log(`record court ${scoreCount}`)
 
+    }
+  }
 
-
-  createTheBoard()
+  createTheBoard(drakePosition)
 
   document.addEventListener('keyup', handleMovements)
+  document.addEventListener('keyup', runTheTrap)
+
 }
 window.addEventListener('DOMContentLoaded', init)
